8842c64fc8bedbc88a0df658f66e8783100af8bb
Chanathip Srithanrat <chanathip@proteus-tech.com>
2014-01-13 02:35:04 +0700

    Use doctest module instead of assert statement

--- potter.py
+++ potter.py
@@ -38,6 +38,44 @@ def match(groups):
 
 
 def price(books):
+    """ Calulate the lowest price of books. 
+    >>> price([])
+    0
+    >>> price([0])
+    8
+    >>> price([1])
+    8
+    >>> price([2])
+    8
+    >>> price([3])
+    8
+    >>> price([4])
+    8
+    >>> price([0, 0])
+    16
+    >>> price([1, 1, 1])
+    24
+    >>> price([0, 1])
+    15.2
+    >>> price([0, 2, 4])
+    21.6
+    >>> price([0, 1, 2, 4])
+    25.6
+    >>> price([0, 1, 2, 3, 4])
+    30.0
+    >>> price([0, 0, 1])
+    23.2
+    >>> price([0, 0, 1, 1])
+    30.4
+    >>> price([0, 0, 1, 2, 2, 3])
+    40.8
+    >>> price([0, 1, 1, 2, 3, 4])
+    38.0
+    >>> price([0, 0, 1, 1, 2, 2, 3, 4])
+    51.2
+    >>> price([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4])
+    141.2
+    """
     result = 0
     price_per_unit = 8
     length_to_rate = {1: 1, 2: 0.95, 3: 0.9, 4: 0.8, 5: 0.75}
@@ -52,25 +90,5 @@ def price(books):
 
 
 if __name__ == '__main__':
-    assert 0 == price([])
-    assert 8 == price([0])
-    assert 8 == price([1])
-    assert 8 == price([2])
-    assert 8 == price([3])
-    assert 8 == price([4])
-    assert 8 * 2 == price([0, 0])
-    assert 8 * 3 == price([1, 1, 1])
-    assert 8 * 2 * 0.95 == price([0, 1])
-    assert 8 * 3 * 0.9 == price([0, 2, 4])
-    assert 8 * 4 * 0.8 == price([0, 1, 2, 4])
-    assert 8 * 5 * 0.75 == price([0, 1, 2, 3, 4])
-    assert 8 + (8 * 2 * 0.95) == price([0, 0, 1])
-    assert 2 * (8 * 2 * 0.95) == price([0, 0, 1, 1])
-    assert (8 * 4 * 0.8) + (8 * 2 * 0.95) == price([0, 0, 1, 2, 2, 3])
-    assert 8 + (8 * 5 * 0.75) == price([0, 1, 1, 2, 3, 4])
-    assert 2 * (8 * 4 * 0.8) == price([0, 0, 1, 1, 2, 2, 3, 4])
-    assert 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8) == price([0, 0, 0, 0, 0,
-                                                            1, 1, 1, 1, 1,
-                                                            2, 2, 2, 2,
-                                                            3, 3, 3, 3, 3,
-                                                            4, 4, 4, 4])
+    import doctest
+    doctest.testmod()
