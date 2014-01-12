346304c767bba790f548d5c2c7c297aa6161d56a
Chanathip Srithanrat <chanathip@proteus-tech.com>
2014-01-13 03:56:33 +0700

    Refactor code, again...

--- potter.py
+++ potter.py
@@ -40,7 +40,9 @@ def price(books):
     result = 0
     price_per_unit = 8
     len_to_rate = {1: 1, 2: 0.95, 3: 0.9, 4: 0.8, 5: 0.75}
-    cheapest_sets = combine(books)
+
+    groups = combine(books)
+    cheapest_sets = combine_cheapest(groups)
 
     for cheapest_set in cheapest_sets:
         cheapest_set_len = len(cheapest_set)
@@ -66,10 +68,11 @@ def combine(books):
         for book in largest_set:
             books.remove(book)
 
-    return match(groups)
+    return groups
 
 
-def match(groups):
+def combine_cheapest(groups):
+    """ 4, 4 is cheaper than 5, 3. """
     sets_of_5, sets_of_3, cheapest_sets = groups
     min_len = min(len(sets_of_5), len(sets_of_3))
 
