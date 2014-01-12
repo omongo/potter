a4d9ab64fe68e45cc67f7d63761c21578b437228
Chanathip Srithanrat <chanathip@proteus-tech.com>
2014-01-13 05:53:34 +0700

    Edit Unit Test

--- test_potter.py
+++ test_potter.py
@@ -27,7 +27,7 @@ class TestPotter(unittest.TestCase):
     def test_edge_cases(self):
         self.assertEqual(2 * (8 * 4 * 0.8), price([0, 0, 1, 1, 2, 2, 3, 4]))
         self.assertEqual(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8), price([0, 0, 0, 0, 0, 
-                                                                    1, 1, 1, 1, 1, 
-                                                                    2, 2, 2, 2, 
-                                                                    3, 3, 3, 3, 3, 
-                                                                    4, 4, 4, 4]))
+                                                                        1, 1, 1, 1, 1, 
+                                                                        2, 2, 2, 2, 
+                                                                        3, 3, 3, 3, 3, 
+                                                                        4, 4, 4, 4]))
